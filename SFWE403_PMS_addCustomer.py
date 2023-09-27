fb33d9176beadd0833d0e41e78ca5cb6c5ac4bcb
gkshah01 <117230542+gkshah01@users.noreply.github.com>
2023-09-27 13:55:38 -0700

    Add files via upload

---
+++ SFWE403_PMS_addCustomer.py
@@ -0,0 +1,42 @@
+import mysql.connector
+from datetime import datetime 
+
+mydb = mysql.connector.connect(
+        host = 'mysql-145311-0.cloudclusters.net',
+        port = '18166',
+        user = 'admin',
+        passwd = 'FcCZds4d',
+        db = 'PMS'
+    )
+
+mycursor = mydb.cursor()    
+
+def AddCustomer():
+   first_name = input("Enter first name:")
+   last_name = input("Enter last name:")
+   date_of_birth = input("Enter DOB as MM/DD/YYYY")
+   DOB = datetime.strptime(date_of_birth, '%m/%d/%Y')
+   address = input("Enter address:")
+   phone = input("Enter phone number:")
+   email = input("Enter email address:")
+   insurance = input("Enter insurance information:")
+   
+   print(first_name, ' ', last_name, ' ', DOB.month, "/", DOB.day, "/", DOB.year, ' ',address, ' ',phone, ' ',email, ' ',insurance, sep= '')
+
+   mycursor.execute("INSERT INTO Customer (lastName, firstName, DOB, Address, phoneNum, email, insurance) VALUES (%s, %s, %s, %s, %s, %s, %s)", (last_name, first_name, DOB, address, phone, email, insurance))
+   mydb.commit
+
+
+
+
+
+if __name__ == '__main__':
+   
+   AddCustomer()
+
+   mycursor.execute("SELECT * FROM Customer")
+   for x in mycursor:
+      print(x)
+ 
+
+
