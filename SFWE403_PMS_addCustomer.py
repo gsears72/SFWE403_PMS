c2ada29e4a52f87dae9e62876f070844303394bc
gkshah01 <117230542+gkshah01@users.noreply.github.com>
2023-09-29 13:20:54 -0700

    Update SFWE403_PMS_addCustomer.py
    added update customer

--- SFWE403_PMS_addCustomer.py
+++ SFWE403_PMS_addCustomer.py
@@ -14,8 +14,8 @@ mycursor = mydb.cursor()
 def AddCustomer():
    first_name = input("Enter first name:")
    last_name = input("Enter last name:")
-   date_of_birth = input("Enter DOB as YYYY-MM-DD")
-   #DOB = datetime.strptime(date_of_birth, '%Y/%m/%d')
+   date_of_birth = input("Enter DOB as MM/DD/YYYY")
+   DOB = datetime.strptime(date_of_birth, '%m/%d/%Y')
    address = input("Enter address:")
    phone = input("Enter phone number:")
    email = input("Enter email address:")
@@ -23,20 +23,77 @@ def AddCustomer():
    
    #print(first_name, ' ', last_name, ' ', DOB.month, "/", DOB.day, "/", DOB.year, ' ',address, ' ',phone, ' ',email, ' ',insurance, sep= '')
 
-   mycursor.execute("INSERT INTO Customer (lastName, firstName, DOB, Address, phoneNum, email, insurance) VALUES (%s, %s, %s, %s, %s, %s, %s)", (last_name, first_name, date_of_birth, address, phone, email, insurance))
+   mycursor.execute("INSERT INTO Customer (lastName, firstName, DOB, Address, phoneNum, email, insurance) VALUES (%s, %s, %s, %s, %s, %s, %s)", (last_name, first_name, DOB, address, phone, email, insurance))
    mydb.commit()
 
 
+def UpdateCustomer():
+   #field is the information field that is being updated
+
+   customerinfo = input("please enter the first and last name and DOB of the patient profile you wish to update (first last mm/dd/yyyy)")
+   customer = customerinfo.split()
+   first = customer[0]
+   last = customer[1]
+   dob = customer[2]
+   #query that will give the corresponding customer id and assign it to the variable ID
+   mycursor.execute("SELECT * FROM Customer WHERE lastName = %s and firstName = %s and DOB = %s", last, first, dob)
+   ID = mycursor.fetchone()
+
+
+   valid = False
+   while valid == False:
+      field = input("Please enter the information field you wish to update (first, last, DOB, address, phone, email, insurance)")
+
+      if field == 'first':
+         valid = True
+         updatedField = input("Please enter the updated information")
+         mycursor.execute("UPDATE Customer SET firstName = %s WHERE Customer_ID = %s", updatedField, ID)
+         mydb.commit()
+      elif field == 'last':
+         valid = True
+         updatedField = input("Please enter the updated information")
+         mycursor.execute("UPDATE Customer SET lastName = %s WHERE Customer_ID = %s", updatedField, ID)
+         mydb.commit()
+      elif field == 'DOB':
+         valid = True
+         updatedField = input("Please enter the updated information")
+         mycursor.execute("UPDATE Customer SET DOB = %s WHERE Customer_ID = %s", updatedField, ID)
+         mydb.commit()
+      elif field == 'phoneNum':
+         valid = True
+         updatedField = input("Please enter the updated information")
+         mycursor.execute("UPDATE Customer SET phoneNum = %s WHERE Customer_ID = %s", updatedField, ID)
+         mydb.commit()
+      elif field == 'email':
+         valid = True
+         updatedField = input("Please enter the updated information")
+         mycursor.execute("UPDATE Customer SET email = %s WHERE Customer_ID = %s", updatedField, ID)
+         mydb.commit()
+      elif field == 'insurance':
+         valid = True
+         updatedField = input("Please enter the updated information")
+         mycursor.execute("UPDATE Customer SET insurance = %s WHERE Customer_ID = %s", updatedField, ID)
+         mydb.commit()
+      else:
+         print("no field found, please try again \n")   
 
 
 
 if __name__ == '__main__':
-   
+   #all of this code is bascially to test that things are working 
    AddCustomer()
 
    mycursor.execute("SELECT * FROM Customer")
    for x in mycursor:
       print(x)
  
+   UpdateCustomer()
+
+   mycursor.execute("SELECT * FROM Customer")
+   for x in mycursor:
+      print(x)
+
+
+
 
 
