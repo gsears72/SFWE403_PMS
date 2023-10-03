c01729e0da83294064c2460532243fbfbba946c0
conortoole <70600055+conortoole@users.noreply.github.com>
2023-10-02 23:36:51 -0700

    Update prescription.py
    almost

--- prescription.py
+++ prescription.py
@@ -1,8 +1,12 @@
 class Prescription:
-    def __init__(self, prescription, customerID, startDate, endDate, medication, quantity):
+    def __init__(self, prescription, customerID, startDate, endDate, medication, quantity, strength, refills, instructions, prescriber):
         self.prescription = prescription
         self.customerID = customerID
         self.startDate = startDate
         self.endDate = endDate
         self.medication = medication
         self.quantity = quantity
+        self.strength = strength
+        self.refills = refills
+        self.instructions = instructions
+        self.prescriber = prescriber
