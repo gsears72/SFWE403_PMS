7fd2ba08f2edfd69db2cb4c4c8572dcfe44b2456
gsears72 <88736762+gsears72@users.noreply.github.com>
2023-11-11 19:43:01 -0700

    Login Fully Fuctional

--- controllers/LoginController.py
+++ controllers/LoginController.py
@@ -1,11 +1,11 @@
 from controllers.DatabaseController import Connect
 
 
-def Login(userID,Password):
+def Login(userID,Password,Secret):
     conn = None
    
-    sql = """SELECT * FROM PMS_Staff WHERE StaffID = %s and password = %s"""
-    values = (userID,Password)
+    sql = """SELECT * FROM PMS_Staff WHERE StaffID = %s and password = %s and highschool = %s"""
+    values = (userID,Password,Secret)
     results = None
 
     try:
@@ -23,3 +23,59 @@ def Login(userID,Password):
         del values,sql,conn
         return results
     
+def Loginstrike (userID):
+    
+    conn = None 
+    sql = """SELECT * FROM PMS_Staff WHERE StaffID = %s"""
+    values = (userID,)
+    results = None
+
+    try:
+        conn = Connect()
+        cursor = conn.cursor()
+        cursor.execute(sql,values)
+        results = cursor.fetchone()
+        strikecount = results[6]
+        if strikecount >=5:
+            sql = "UPDATE `PMS_Staff` SET `lockout` = '1' WHERE StaffID = %s"
+            cursor.execute(sql,values)
+            conn.commit() 
+        else:
+            strikecount = strikecount+1
+            sql = "UPDATE `PMS_Staff` SET `strikecount` = %s WHERE StaffID = %s"
+            values = (strikecount,userID)
+            cursor.execute(sql,values)
+            conn.commit() 
+        cursor.close
+        conn.close
+
+    except:
+       print("Error retriving Login")
+
+    finally:
+        del sql,conn,values
+        return results
+    
+    
+def Resetstrike (userID):
+    
+    conn = None
+   
+    sql = """UPDATE `PMS_Staff` SET `strikecount` = '0' WHERE StaffID = %s"""
+    values = (userID,)
+    results = None
+
+    try:
+        conn = Connect()
+        cursor = conn.cursor()
+        cursor.execute(sql,values)
+        conn.commit()
+        cursor.close
+        conn.close
+
+    except:
+        print("Error retriving Login")
+
+    finally:
+        del values,sql,conn
+        return results
