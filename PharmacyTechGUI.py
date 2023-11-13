b638176cba4a7c5714e5166a0d2a86a2dc861e5f
conortoole <70600055+conortoole@users.noreply.github.com>
2023-11-12 20:43:03 -0700

    Add files via upload

--- PharmacyTechGUI.py
+++ PharmacyTechGUI.py
@@ -1,51 +1,63 @@
 import customtkinter as tk
 #import addCustomerView as acv
 
+def open_pharmacistTechGUI(app):
+    CashierHome = tk.CTkToplevel()
+    CashierHome.geometry("700x200")
+    CashierHome.title("Pharmacy Technician Homepage")
 
+    #should add instant check for low stock or expired notifications
+    def LogOut():
+        app.deiconify()
+        CashierHome.destroy()
 
-CashierHome = tk.CTk()
-CashierHome.geometry("700x200")
-CashierHome.title("Pharmacy Technician Homepage")
+    AddCustomerButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Add Customer",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        #command = acv.open_addCustomerView()
+    )
 
-#should add instant check for low stock or expired notifications
+    UpdateCustomerButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Update Customer",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        #command = open updatecustomer view 
+    )
 
-AddCustomerButton = tk.CTkButton(
-    master = CashierHome,
-    text = "Add Customer",
-    width = 200,
-    height = 50,
-    #bg = "blue",
-    #fg = "yellow",
-    #font = 10,
-    #command = acv.open_addCustomerView()
-)
+    DeleteCustomerButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Delete Customer",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        #command = open deletecustomer view 
+    )
 
-UpdateCustomerButton = tk.CTkButton(
-    master = CashierHome,
-    text = "Update Customer",
-    width = 200,
-    height = 50,
-    #bg = "blue",
-    #fg = "yellow",
-    #font = 10,
-    #command = open updatecustomer view 
-)
+    LogOutButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Log Out",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        command = LogOut
+    )
 
-DeleteCustomerButton = tk.CTkButton(
-    master = CashierHome,
-    text = "Delete Customer",
-    width = 200,
-    height = 50,
-    #bg = "blue",
-    #fg = "yellow",
-    #font = 10,
-    #command = open deletecustomer view 
-)
+    AddCustomerButton.grid(row = 0, column = 0, padx=10, pady=10)
+    UpdateCustomerButton.grid(row = 0, column = 20, padx=10, pady=10)
+    DeleteCustomerButton.grid(row = 0, column = 40, padx=10, pady=10)
+    LogOutButton.grid(row = 0, column = 20, padx=10, pady=10)
 
-AddCustomerButton.grid(row = 0, column = 0, padx=10, pady=10)
-UpdateCustomerButton.grid(row = 0, column = 20, padx=10, pady=10)
-DeleteCustomerButton.grid(row = 0, column = 40, padx=10, pady=10)
-
-
-
-CashierHome.mainloop()
+    #CashierHome.mainloop()
