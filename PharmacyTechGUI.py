a7a2e8317d28f3c3622a6b8a5f311a1de686db13
gkshah01 <117230542+gkshah01@users.noreply.github.com>
2023-11-19 22:31:55 -0700

    Update PharmacyTechGUI.py

--- PharmacyTechGUI.py
+++ PharmacyTechGUI.py
@@ -1,51 +1,68 @@
 import customtkinter as tk
 #import addCustomerView as acv
+import expiration as exp 
+from tkinter import messagebox
+
+def open_pharmacistTechGUI(app):
+    CashierHome = tk.CTkToplevel()
+    CashierHome.geometry("700x200")
+    CashierHome.title("Pharmacy Technician Homepage")
+
+    #should add instant check for low stock or expired notifications
+    def LogOut():
+        app.deiconify()
+        CashierHome.destroy()
+
+    AddCustomerButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Add Customer",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        #command = acv.open_addCustomerView()
+    )
+
+    UpdateCustomerButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Update Customer",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        #command = open updatecustomer view 
+    )
+
+    DeleteCustomerButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Delete Customer",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        #command = open deletecustomer view 
+    )
+
+    LogOutButton = tk.CTkButton(
+        master = CashierHome,
+        text = "Log Out",
+        width = 200,
+        height = 50,
+        #bg = "blue",
+        #fg = "yellow",
+        #font = 10,
+        command = LogOut
+    )
+
+    AddCustomerButton.grid(row = 0, column = 0, padx=10, pady=10)
+    UpdateCustomerButton.grid(row = 0, column = 20, padx=10, pady=10)
+    DeleteCustomerButton.grid(row = 0, column = 40, padx=10, pady=10)
+    LogOutButton.grid(row = 0, column = 20, padx=10, pady=10)
+
+    messagebox.showwarning("WARNING: The following medications are EXPIRED. Do NOT use and DISPOSE IMMEDIATELY", exp.Expired())
 
 
-
-CashierHome = tk.CTk()
-CashierHome.geometry("700x200")
-CashierHome.title("Pharmacy Technician Homepage")
-
-#should add instant check for low stock or expired notifications
-
-AddCustomerButton = tk.CTkButton(
-    master = CashierHome,
-    text = "Add Customer",
-    width = 200,
-    height = 50,
-    #bg = "blue",
-    #fg = "yellow",
-    #font = 10,
-    #command = acv.open_addCustomerView()
-)
-
-UpdateCustomerButton = tk.CTkButton(
-    master = CashierHome,
-    text = "Update Customer",
-    width = 200,
-    height = 50,
-    #bg = "blue",
-    #fg = "yellow",
-    #font = 10,
-    #command = open updatecustomer view 
-)
-
-DeleteCustomerButton = tk.CTkButton(
-    master = CashierHome,
-    text = "Delete Customer",
-    width = 200,
-    height = 50,
-    #bg = "blue",
-    #fg = "yellow",
-    #font = 10,
-    #command = open deletecustomer view 
-)
-
-AddCustomerButton.grid(row = 0, column = 0, padx=10, pady=10)
-UpdateCustomerButton.grid(row = 0, column = 20, padx=10, pady=10)
-DeleteCustomerButton.grid(row = 0, column = 40, padx=10, pady=10)
-
-
-
-CashierHome.mainloop()
+    #CashierHome.mainloop()
